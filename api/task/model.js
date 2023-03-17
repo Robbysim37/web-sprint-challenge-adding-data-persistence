ae3ddb8a77f51cfb8127801dc65b67ffd39ef5d7
Robby Lewis <Robbysim37@gmail.com>
2023-03-17 18:12:49 -0400

    finished

--- api/task/model.js
+++ api/task/model.js
@@ -2,13 +2,36 @@
 const db = require("../../data/dbConfig")
 
 const getTasks = async () => {
-const tasksArr = await db("tasks as t")
-.leftJoin(`projects as p`,`t.project_id`,`p.project_id`)
 
+    const tasksArr = await db("tasks as t")
+    .leftJoin(`projects as p`,`t.project_id`,`p.project_id`)
 
-return tasksArr
+    let tasksSchema = tasksArr.map(el => {
+        return {
+            task_id: el.task_id,
+            task_description: el.task_description,
+            task_notes: el.task_notes,
+            task_completed: el.task_completed ? true : false,
+            project_name: el.project_name,
+            project_description: el.project_description,
+        }
+    })
+    return tasksSchema
+}
+
+const insertTask = async (task) => {
+    await db("tasks").insert(task)
+    const taskSchema = {
+        task_id: task.project_id,
+        task_description: task.task_description,
+        task_notes: task.task_notes || null,
+        task_completed: task.task_completed ? true : false,
+        project_id: task.project_id,
+    }
+    return taskSchema
 }
 
 module.exports = {
     getTasks,
+    insertTask,
 }
